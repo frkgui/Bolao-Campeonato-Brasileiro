7b570b3462db9b50fc8cb1063df1f12ce64a7446
carlos <carlosbernardo774@gmail.com>
2024-04-11 14:18:59 -0300

    make main menu

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+
+        System.out.println("""
+                Seja Bem Vindo a Simulação e Bolão de Campeonato de Futebol!
+                
+                """);
         Time timeUm = new Time("Grêmio");
         Time timeDois = new Time("BInternacional");
 
         Campeonato campeonato = new Campeonato("Brasileirão");
+
+
 
     }
 }
